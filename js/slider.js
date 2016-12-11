0d0bbd8e90d263af2289bc70aa50d2c3b108fdc9
mkasumi <morita@appleple.com>
2016-12-11 15:33:40 +0900

    基本テンプレートの用意・右左に送るところまで

---
+++ js/slider.js
@@ -0,0 +1,37 @@
+(function( $ ) {
+	$.fn.slide = function() {
+
+	var items = this.find('.slider-item').length;
+
+	// データ属性で何番目か取得する
+	$('.slider-item').each(function(i){
+		$(this).attr('data-index',i);
+	});
+
+	var width = this.children().width();
+	var itemsWidth = items * width;
+	$('.slider-item').width(width);
+	$('.slider-list').width(itemsWidth);
+
+	$('.slider-item:first-child').addClass('active');
+
+	$('.slider-arrow-next button').click(function(){
+		var amountItem = $('.slider-list').find('.active').attr('data-index');
+		var amount = parseInt(amountItem)+1;
+		console.log('amount' + amount);
+		$('.slider-list').css('transform','translateX(-'+ width * amount +'px)');
+		$('.active').removeClass('active').next().addClass('active');
+		
+	});
+
+	$('.slider-arrow-prev').click(function(){
+		var amountItem = $('.slider-list').find('.active').attr('data-index');
+		var amount = parseInt(amountItem)-1;
+		console.log('amount' + amount);
+		$('.slider-list').css('transform','translateX(-'+ width * amount +'px)');
+		$('.active').removeClass('active').next().addClass('active');
+
+	});
+
+	};
+})( jQuery );
