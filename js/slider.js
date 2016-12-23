12124224e81569c80eb5482ea74dbaf2e5d076e5
mkasumi <morita@appleple.com>
2016-12-23 12:47:09 +0900

    クラス名を変更できる箇所を追加

--- js/slider.js
+++ js/slider.js
@@ -1,56 +1,59 @@
 (function( $ ) {
 	$.fn.slide = function(option) {
 		var defaults = {
-			slider:'.slider-list',
+			slider:'slider-list',
+			sliderItem: 'slider-item',
+			dotsItem: 'slider-dots',
+			arrowItem: 'slider-arrow',
 			dots: 'true',
 			arrows: 'true'
 		};
 		var option = $.extend({}, defaults, option);
 
 		var $self = $(this);
-		var $ul = $(defaults.slider,this);
-		var items = this.find('.slider-item').length;
+		var $ul = $('.' + defaults.slider,this);
+		var items = this.find('.' + defaults.sliderItem).length;
 		var moveTo = function(amount,direction){
 			$ul.css('transform','translateX(-'+ width * amount +'px)');
 			if (direction === 'next') {
-				$('.slider-item.active').removeClass('active').next().addClass('active');
+				$('.' + defaults.sliderItem +'.active').removeClass('active').next().addClass('active');
 			} else if (direction === 'prev') {
-				$('.slider-item.active').removeClass('active').prev().addClass('active');
+				$('.' + defaults.sliderItem +'.active').removeClass('active').prev().addClass('active');
 			} else if (direction === 'none') {
-				$('.slider-item.active').removeClass('active').addClass('active');
+				$('.' + defaults.sliderItem +'.active').removeClass('active').addClass('active');
 			}
 		}
 		var arrowDisable = function(){
-			if ($('.slider-item.active').is(':last-child')){
-				$('.slider-arrow-prev button').prop('disabled',false);
-				$('.slider-arrow-next button').prop('disabled',true);
-			} else if ($('.slider-item.active').is(':first-child')){
-				$('.slider-arrow-next button').prop('disabled',false);
-				$('.slider-arrow-prev button').prop('disabled',true);
+			if ($('.' + defaults.sliderItem +'.active').is(':last-child')){
+				$('.'+ defaults.arrowItem +'-prev button').prop('disabled',false);
+				$('.'+ defaults.arrowItem +'-next button').prop('disabled',true);
+			} else if ($('.' + defaults.sliderItem +'.active').is(':first-child')){
+				$('.'+ defaults.arrowItem +'-next button').prop('disabled',false);
+				$('.'+ defaults.arrowItem +'-prev button').prop('disabled',true);
 			} else {
-				$('.slider-arrow-prev button').prop('disabled',false);
-				$('.slider-arrow-next button').prop('disabled',false);
+				$('.'+ defaults.arrowItem +'-prev button').prop('disabled',false);
+				$('.'+ defaults.arrowItem +'-next button').prop('disabled',false);
 			}
 		}
 
 		// データ属性で何番目か取得する
-		$('.slider-item',this).each(function(i){
+		$('.' + defaults.sliderItem,this).each(function(i){
 			$(this).attr('data-index',i);
 		});
 
 		// 幅の取得
 		var width = this.children().width();
 		var itemsWidth = items * width;
-		$('.slider-item',this).width(width);
-		$('.slider-list',this).width(itemsWidth);
+		$('.' + defaults.sliderItem,this).width(width);
+		$('.' + defaults.slider,this).width(itemsWidth);
 
-		$('.slider-item:first-child').addClass('active');
+		$('.' + defaults.sliderItem +':first-child').addClass('active');
 
 		if ( defaults.arrows === 'true') {
-			$(this).append('<ul class="slider-arrow"><li class="slider-arrow-prev"><button type="button">前へ</button></li><li class="slider-arrow-next"><button type="button">次へ</button></li></ul>');
+			$(this).append('<ul class="'+ defaults.arrowItem +'"><li class="slider-arrow-prev"><button type="button">前へ</button></li><li class="slider-arrow-next"><button type="button">次へ</button></li></ul>');
 			// 次に送る
-			$('.slider-arrow-next button').click(function(){
-				$('.slider-arrow-prev button').prop('disabled',false);
+			$('.'+ defaults.arrowItem +'-next button').click(function(){
+				$('.'+ defaults.arrowItem +'-prev button').prop('disabled',false);
 				var amountItem = $ul.find('.active').attr('data-index');
 				var amount = parseInt(amountItem)+1;
 				var direction = 'next';
@@ -59,8 +62,8 @@
 			});
 
 			// 前へ送る
-			$('.slider-arrow-prev button').click(function(){
-				$('.slider-arrow-next button').prop('disabled',false);
+			$('.'+ defaults.arrowItem +'-prev button').click(function(){
+				$('.'+ defaults.arrowItem +'-next button').prop('disabled',false);
 				var amountItem = $ul.find('.active').attr('data-index');
 				var amount = parseInt(amountItem)-1;
 				var direction = 'prev';
@@ -72,21 +75,21 @@
 		//ドットナビゲーション
 		if ( defaults.dots === 'true') {
 			// ドットナビゲーションを追加する
-			$(this).append('<ul class="slider-dots"></ul>');
+			$(this).append('<ul class="'+ defaults.dotsItem +'"></ul>');
 			// データ属性で何番目か取得する
-			$('.slider-item',this).each(function(i){
-				$('.slider-dots').append('<li><button type="button" data-dots-item="'+i+'">'+i+'</button></li>');
+			$('.' + defaults.sliderItem,this).each(function(i){
+				$('.' +defaults.dotsItem).append('<li><button type="button" data-dots-item="'+i+'">'+i+'</button></li>');
 			});
-			$('.slider-dots button',this).click(function(){
-				$('.slider-dots button').removeClass('active');
+			$('.' +defaults.dotsItem + ' button',this).click(function(){
+				$('.' +defaults.dotsItem + ' button').removeClass('active');
 				$(this).addClass('active');
 
 				var selectedItem = $(this).attr('data-dots-item');
-				$('.slider-item').removeClass('active');
+				$('.' + defaults.sliderItem).removeClass('active');
 
-				var $item = $('.slider-item').removeClass('active');
-				var attr = $('.slider-item').attr('data-index');
-				$('.slider-item')
+				var $item = $('.' + defaults.sliderItem).removeClass('active');
+				var attr = $('.' + defaults.sliderItem).attr('data-index');
+				$('.' + defaults.sliderItem)
 				.removeClass('active')
 				.each(function(){
 					if($(this).attr("data-index") === selectedItem){
