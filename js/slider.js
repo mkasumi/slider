313618016e5baec7f3111092105f56518438b119
mkasumi <morita@appleple.com>
2016-12-23 16:40:26 +0900

    左右ナビゲーションをクリックしたらドットナビゲーションがactiveになるように修正

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,5 @@
 (function( $ ) {
+	'use strict';
 	$.fn.slide = function(option) {
 		var defaults = {
 			slider:'slider-list',
@@ -8,52 +9,55 @@
 			dots: 'true',
 			arrows: 'true'
 		};
+
 		var option = $.extend({}, defaults, option);
 
 		var $self = $(this);
-		var $ul = $('.' + defaults.slider,this);
-		var items = this.find('.' + defaults.sliderItem).length;
+		var $ul = $('.' + option.slider,$self);
+		var items = this.find('.' + option.sliderItem).length;
 		var moveTo = function(amount,direction){
 			$ul.css('transform','translateX(-'+ width * amount +'px)');
 			if (direction === 'next') {
-				$('.' + defaults.sliderItem +'.active').removeClass('active').next().addClass('active');
+				$('.' + option.sliderItem +'.active',$self).removeClass('active').next().addClass('active');
+				$('.'+ option.dotsItem + ' button.active',$self).removeClass('active').parent().next().find('button').addClass('active');
 			} else if (direction === 'prev') {
-				$('.' + defaults.sliderItem +'.active').removeClass('active').prev().addClass('active');
+				$('.' + option.sliderItem +'.active',$self).removeClass('active').prev().addClass('active');
+				$('.'+ option.dotsItem + ' button.active',$self).removeClass('active').parent().prev().find('button').addClass('active');
 			} else if (direction === 'none') {
-				$('.' + defaults.sliderItem +'.active').removeClass('active').addClass('active');
+				$('.' + option.sliderItem +'.active').removeClass('active').addClass('active');
 			}
 		}
 		var arrowDisable = function(){
-			if ($('.' + defaults.sliderItem +'.active').is(':last-child')){
-				$('.'+ defaults.arrowItem +'-prev button',$self).prop('disabled',false);
-				$('.'+ defaults.arrowItem +'-next button',$self).prop('disabled',true);
-			} else if ($('.' + defaults.sliderItem +'.active').is(':first-child')){
-				$('.'+ defaults.arrowItem +'-next button',$self).prop('disabled',false);
-				$('.'+ defaults.arrowItem +'-prev button',$self).prop('disabled',true);
+			if ($('.' + option.sliderItem +'.active').is(':last-child')){
+				$('.'+ option.arrowItem +'-prev button',$self).prop('disabled',false);
+				$('.'+ option.arrowItem +'-next button',$self).prop('disabled',true);
+			} else if ($('.' + option.sliderItem +'.active').is(':first-child')){
+				$('.'+ option.arrowItem +'-next button',$self).prop('disabled',false);
+				$('.'+ option.arrowItem +'-prev button',$self).prop('disabled',true);
 			} else {
-				$('.'+ defaults.arrowItem +'-prev button',$self).prop('disabled',false);
-				$('.'+ defaults.arrowItem +'-next button',$self).prop('disabled',false);
+				$('.'+ option.arrowItem +'-prev button',$self).prop('disabled',false);
+				$('.'+ option.arrowItem +'-next button',$self).prop('disabled',false);
 			}
 		}
 
 		// データ属性で何番目か取得する
-		$('.' + defaults.sliderItem,this).each(function(i){
+		$('.' + option.sliderItem,this).each(function(i){
 			$(this).attr('data-index',i);
 		});
 
 		// 幅の取得
 		var width = this.children().width();
 		var itemsWidth = items * width;
-		$('.' + defaults.sliderItem,this).width(width);
-		$('.' + defaults.slider,this).width(itemsWidth);
+		$('.' + option.sliderItem,this).width(width);
+		$('.' + option.slider,this).width(itemsWidth);
 
-		$('.' + defaults.sliderItem +':first-child').addClass('active');
+		$('.' + option.sliderItem +':first-child').addClass('active');
 
-		if ( defaults.arrows === 'true') {
-			$(this).append('<ul class="'+ defaults.arrowItem +'"><li class="slider-arrow-prev"><button type="button">前へ</button></li><li class="slider-arrow-next"><button type="button">次へ</button></li></ul>');
+		if ( option.arrows === 'true') {
+			$(this).append('<ul class="'+ option.arrowItem +'"><li class="slider-arrow-prev"><button type="button">前へ</button></li><li class="slider-arrow-next"><button type="button">次へ</button></li></ul>');
 			// 次に送る
-			$('.'+ defaults.arrowItem +'-next button',this).click(function(){
-				$('.'+ defaults.arrowItem +'-prev button').prop('disabled',false);
+			$('.'+ option.arrowItem +'-next button',this).click(function(){
+				$('.'+ option.arrowItem +'-prev button').prop('disabled',false);
 				var amountItem = $ul.find('.active').attr('data-index');
 				var amount = parseInt(amountItem)+1;
 				var direction = 'next';
@@ -62,8 +66,8 @@
 			});
 
 			// 前へ送る
-			$('.'+ defaults.arrowItem +'-prev button',this).click(function(){
-				$('.'+ defaults.arrowItem +'-next button').prop('disabled',false);
+			$('.'+ option.arrowItem +'-prev button',this).click(function(){
+				$('.'+ option.arrowItem +'-next button').prop('disabled',false);
 				var amountItem = $ul.find('.active').attr('data-index');
 				var amount = parseInt(amountItem)-1;
 				var direction = 'prev';
@@ -73,23 +77,26 @@
 		}
 
 		//ドットナビゲーション
-		if ( defaults.dots === 'true') {
+		if ( option.dots === 'true') {
 			// ドットナビゲーションを追加する
-			$(this).append('<ul class="'+ defaults.dotsItem +'"></ul>');
+			$(this).append('<ul class="'+ option.dotsItem +'"></ul>');
+
 			// データ属性で何番目か取得する
-			$('.' + defaults.sliderItem,this).each(function(i){
-				$('.' +defaults.dotsItem,$self).append('<li><button type="button" data-dots-item="'+i+'">'+i+'</button></li>');
+			$('.' + option.sliderItem,$self).each(function(i){
+				$('.' +option.dotsItem,$self).append('<li><button type="button" data-dots-item="'+i+'">'+i+'</button></li>');
 			});
-			$('.' +defaults.dotsItem + ' button',this).click(function(){
-				$('.' +defaults.dotsItem + ' button').removeClass('active');
+			$('.' + option.dotsItem +' li:first-child button',$self).addClass('active');
+
+			$('.' +option.dotsItem + ' button',$self).click(function(){
+				$('.' +option.dotsItem + ' button',$self).removeClass('active');
 				$(this).addClass('active');
 
 				var selectedItem = $(this).attr('data-dots-item');
-				$('.' + defaults.sliderItem).removeClass('active');
+				$('.' + option.sliderItem,self).removeClass('active');
 
-				var $item = $('.' + defaults.sliderItem).removeClass('active');
-				var attr = $('.' + defaults.sliderItem).attr('data-index');
-				$('.' + defaults.sliderItem)
+				var $item = $('.' + option.sliderItem,$self).removeClass('active');
+				var attr = $('.' + option.sliderItem,$self).attr('data-index');
+				$('.' + option.sliderItem,$self)
 				.removeClass('active')
 				.each(function(){
 					if($(this).attr("data-index") === selectedItem){
